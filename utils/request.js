ec9f6f83632abb612f79a6ae1c4e85c75392cda7
c29tZSBvbmU <86731433+c29tZSBvbmU@users.noreply.github.com>
2022-07-14 00:18:47 +0800

    更改api地址

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const service = axios.create({
-    //https://webapi.nn.com
-    baseURL: process.env.VUE_APP_BASE_API || "https://webapi.nn.com", // url = base url + request url
+    //https://webapi.leigod.com
+    baseURL: process.env.VUE_APP_BASE_API || "https://webapi.leigod.com", // url = base url + request url
     // withCredentials: true, // send cookies when cross-domain requests
     timeout: 5000 // request timeout
 })
