f12b6a952adfd837353e37c31dc53a1a28615a93
c29tZSBvbmU <86731433+c29tZSBvbmU@users.noreply.github.com>
2025-11-29 22:35:03 +0800

    Update request.js

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const service = axios.create({
     //https://webapi.leigod.com
-    baseURL: process.env.VUE_APP_BASE_API || "https://webapi.leigod.com", // url = base url + request url
+    baseURL: process.env.VUE_APP_BASE_API || "https://vip.leigod.com", // url = base url + request url
     // withCredentials: true, // send cookies when cross-domain requests
     timeout: 5000 // request timeout
 })
